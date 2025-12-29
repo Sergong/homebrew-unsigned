0224f43a799de84f6374273f469cfd09e2075712
serge.meeuwsen@gmail.com <serge.meeuwsen@gmail.com>
2025-12-29 17:47:56 +0000

    First commit

---
+++ Casks/inkscape.rb
@@ -0,0 +1,37 @@
+cask "inkscape" do
+  # 1. Define architecture-specific variables
+  # Inkscape uses different URL paths for M1/M2/M3 (dmg-arm64) vs Intel (dmg)
+  arch arm: "dmg-arm64", intel: "dmg"
+
+  version "1.4"
+  sha256 :no_check # We skip the check initially; the automation will fill this in
+
+  # 2. Use the STABLE redirect URL
+  # Instead of guessing "Inkscape-1.4.dmg", we hit the download endpoint.
+  # "dl/" at the end tells the site to trigger the file download.
+  url "https://inkscape.org/release/inkscape-#{version}/mac-os-x/#{arch}/dl/"
+
+  name "Inkscape"
+  desc "Vector graphics editor"
+  homepage "https://inkscape.org/"
+
+  # 3. Livecheck updates
+  # We check the main release page to find the current version number (e.g., "1.4")
+  livecheck do
+    url "https://inkscape.org/release/inkscape-1.4/"
+    regex(/Inkscape\s+v?(\d+(?:\.\d+)+)/i)
+  end
+
+  conflicts_with cask: "inkscape-preview"
+  depends_on macos: ">= :high_sierra"
+
+  app "Inkscape.app"
+  binary "#{appdir}/Inkscape.app/Contents/MacOS/inkscape"
+
+  zap trash: [
+    "~/.config/inkscape",
+    "~/Library/Application Support/Inkscape",
+    "~/Library/Preferences/org.inkscape.Inkscape.plist",
+  ]
+end
+
---
+++ Casks/librewolf.rb
@@ -0,0 +1,33 @@
+cask "librewolf" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "134.0-1" # This line gets updated by the bot
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000" # Updated by bot
+
+  url "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/#{version}/librewolf-#{version}-macos-#{arch}-package.dmg",
+      verified: "gitlab.com/"
+
+  name "LibreWolf"
+  desc "Web browser"
+  homepage "https://librewolf.net/"
+
+  livecheck do
+    url "https://gitlab.com/api/v4/projects/44042130/releases"
+    regex(/librewolf[._-]v?(\d+(?:\.\d+)+(?:-\d+)?)[._-]macos[._-]arm64/i)
+    strategy :json do |json, regex|
+      json.map do |release|
+        asset = release["assets"]["links"].find { |l| l["name"]&.match?(regex) }
+        asset["name"][regex, 1] if asset
+      end
+    end
+  end
+
+  app "LibreWolf.app"
+
+  zap trash: [
+    "~/Library/Application Support/LibreWolf",
+    "~/Library/Caches/LibreWolf",
+    "~/Library/Preferences/io.gitlab.librewolf-community.librewolf.plist",
+  ]
+end
+
